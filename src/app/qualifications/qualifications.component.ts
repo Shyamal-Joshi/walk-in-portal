8d8329b6afe6a1ded86b8e6dc4a66e74ff1ef846
Shyamal Zeus <shyamal.joshi@zeuslearning.com>
2024-02-08 20:00:00 +0530

    educational qualifications and fresher qualifications completed

---
+++ src/app/qualifications/qualifications.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-qualifications',
+  standalone: true,
+  imports: [MatIconModule,CommonModule,FormsModule],
+  templateUrl: './qualifications.component.html',
+  styleUrl: './qualifications.component.scss'
+})
+export class QualificationsComponent  {
+  isEducationalQualificationVisible:boolean = false;
+  isProfessionalQualificationVisible:boolean = false;
+  isExperienced:boolean = false;
+
+  
+  toogleEducationalQualification(){
+    this.isEducationalQualificationVisible = !this.isEducationalQualificationVisible;
+  }
+  toogleProfessionalQualification(){
+    this.isProfessionalQualificationVisible = !this.isProfessionalQualificationVisible;
+  }
+
+  toggleExperience(){
+    this.isExperienced=!this.isExperienced;
+  }
+
+  
+}
--- src/app/user-registration/user-registration.component.ts
+++ src/app/user-registration/user-registration.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { PersonalInformationComponent } from '../personal-information/personal-information.component';
+import { QualificationsComponent } from '../qualifications/qualifications.component';
 
 @Component({
   selector: 'app-user-registration',
   standalone: true,
-  imports: [MatIconModule,CommonModule,PersonalInformationComponent],
+  imports: [MatIconModule,CommonModule,PersonalInformationComponent,QualificationsComponent],
   templateUrl: './user-registration.component.html',
   styleUrl: './user-registration.component.scss'
 })
